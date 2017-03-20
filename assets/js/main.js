6a7a8956e2fde087a2d6c64ce6a842be18ae084e
Peter Antonius <peter.antonius@whitespace.se>
2017-03-20 11:36:34 +0100

    Added initial JavaScript

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import components from './components/core';
+import toggle from './components/toggle';
+
+// Kickstart the components on the page
+components.enhance();
+
+import global from './global';
