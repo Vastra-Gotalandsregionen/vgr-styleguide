1d9d20e88d5c42738bad9da492d0c344cd64e508
Peter Antonius <peter.antonius@whitespace.se>
2017-09-27 17:49:11 +0200

    New JavaScript handling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,3 @@
-'use strict';
+import './modules'
 
-import components from './components/core';
-import toggle from './components/toggle';
-
-// Kickstart the components on the page
-components.enhance();
-
-import global from './global';
-
-// Add component from Components
-import Awesomplete from 'awesomplete';
+import whatInput from 'what-input'
---
+++ assets/js/modules/toggle.js
@@ -0,0 +1,34 @@
+export default class toggleButton {
+    constructor(element) {
+
+        let menuId = element.getAttribute('aria-controls');
+        if (!menuId) {
+            return;
+        }
+
+        let menu = document.getElementById(menuId);
+        if (!menu) {
+            return;
+        }
+
+        element.addEventListener('click', (e) => {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+
+            let transitionTime = element.getAttribute('data-transition-time');
+            if (transitionTime) {
+                menu.classList.add('animating');
+                setTimeout(() => menu.classList.remove('animating'), parseInt(transitionTime));
+            }
+
+            let expanded = menu.getAttribute('aria-expanded');
+            if (expanded === 'true') {
+                menu.setAttribute('aria-expanded', 'false');
+                element.removeAttribute('data-expanded');
+            } else {
+                menu.setAttribute('aria-expanded', 'true');
+                element.setAttribute('data-expanded', '');
+            }
+        });
+    }
+}
